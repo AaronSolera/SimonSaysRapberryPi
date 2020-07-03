44a6becc5343d25ede046389f16a96cef5c52c1f
marcf98 <32026019+marcf98@users.noreply.github.com>
2020-07-02 20:26:04 -0600

    Levels
    For each good one, the speed decreases

--- controlador/controlador.ino
+++ controlador/controlador.ino
@@ -1,6 +1,8 @@
 long randNumber;
 int confirmationBit=0;
 String order;
+int sleep=1000;
+int flag=1;
 
 void setup() {
   Serial.begin(9600);
@@ -22,7 +24,7 @@ void loop() {
     randNumber = random(2, 6);
     order.concat(String(randNumber));
     digitalWrite(randNumber, HIGH);
-    delay(1000);
+    delay(sleep);
     digitalWrite(randNumber, LOW);
     delay(100);
   }
@@ -30,16 +32,27 @@ void loop() {
   Serial.println(order);
   //reads the serial and waits for the confirmation bit
   confirmationBit = Serial.read();
-  while(confirmationBit==-1){
+  flag=1;
+  while(flag){
+    if (Serial.available() > 0) {
+    // read the incoming byte:
     confirmationBit = Serial.read();
-    if(confirmationBit==1){
-      break;
+    }    
+    if(confirmationBit==49){//49 is 1
+      flag=0;
     }
-    delay(1);
+    else if(confirmationBit==50){//50 is 2
+      sleep=1000;
+      flag=0;
+    }
+    delay(10);
   }
   delay(1000);
   //clear the buffer from garbage values
   while(Serial.available() > 0) {
     char t = Serial.read();
+  }
+  if(sleep>100){
+    sleep = sleep - 50;
   }
 }
