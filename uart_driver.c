edac78814e03c27284948f595eb17309feb1d6e1
AaronSolera <aaron.smartinez1997@gmail.com>
2020-07-30 14:27:25 -0600

    UART driver update. Almost done. Debugging fase.

--- uart_driver.c
+++ uart_driver.c
@@ -4,7 +4,15 @@
 #include <fcntl.h> //file ops
 #include <unistd.h> //usleep
 #include <string.h>
+#include <math.h>
 
+// UART clock frequency
+#define FUARTCLK 48000000
+
+#define ALL 0
+#define CLEAR 0x0
+#define SET 0xFFFFFFFF
+// Macros for line control register
 #define SPS 7
 #define WLEN 5
 #define FEN 4
@@ -13,67 +21,138 @@
 #define PEN 1
 #define BRK 0
 
+// Macros for control register
+#define RXE 9
+#define TXE 8
+#define UARTEN 0
+
+// Macros for interrupt mask set clear register
+#define TXIM 5
+#define RXIM 4
+
 typedef volatile unsigned int * reg;
-//Static base
-static unsigned UART_BASE = 0x7E20100;
-//Regs pointers
+// Static base
+static unsigned UART_BASE = 0x3f201000;
+
+// Regs pointers
 reg data_rg;
 reg line_control_rg;
-/*Function prototypes*/
-void uartInit();
-void uartSendChar(char c);
-void uartSetControlRg(int field_name, int bits);
-void setBit(int * rgt, int n);
-void clearBit(int * rgt, int n);
-void writeBits(int * rgt, int bits, int n);
+reg control_rg;
+reg flags_rg;
+reg int_baudrate_rg;
+reg frac_baudrate_rg;
+reg interrupt_mask_set_clear_rg;
+reg raw_interrupt_status_rg;
+reg interrupt_clear_rg;
+// Function prototypes
+void uartInit(int baudrate);
+char uartReadChar();
+void uartSetBaudrate(int baudrate);
+void uartSetLineControlReg(int field_name, int bits);
+void uartSetControlReg(int field_name, int bits);
+void uartSetInterruptMaskSetClearReg(int field_name, int bits);
+void uartSetInterruptClearReg(int field_name, int bits);
+void uartTransmitData(char data);	
+char uartReceiveData();
+void setBit(reg rgt, int n);
+void clearBit(reg rgt, int n);
+void writeBits(reg rgt, int bits, int n);
+int  readBits(reg rgt, int n_read, int position);
 
 int main(){
-	int r = 7;
-	writeBits(&r, 0x2, STP2);
-	printf("%i\n", r);
+	printf("Holi soy el driver de UART (:D)\n");
 	return 0;
 }
 
-//Initialize pointers: performs memory mapping, exits if mapping fails
-void uartInit(){
-	/*
+// Initialize pointers: performs memory mapping, exits if mapping fails
+void uartInit(int baudrate){
 	int fd = -1;
-	//Loading /dev/mem into a file
+	// Loading /dev/mem into a file
 	if ((fd = open("/dev/mem", O_RDWR, 0)) == -1) err(1, "Error opening /dev/mem");
-	//Mapping GPIO base physical address
-	data_rg = (unsigned int*) mmap(0, getpagesize(), PROT_WRITE | PROT_READ, MAP_SHARED, fd, GPIO_BASE);
-	//Check for mapping errors
+	// Mapping GPIO base physical address
+	data_rg = (unsigned int*) mmap(0, getpagesize(), PROT_WRITE | PROT_READ, MAP_SHARED, fd, UART_BASE);
+	// Check for mapping errors
 	if (data_rg == MAP_FAILED) errx(1, "Error during mapping UART");
-	//Set regs pointers
+	// Setting regs pointers
+	flags_rg = data_rg + 0x18;
+	int_baudrate_rg = data_rg + 0x24;
+	frac_baudrate_rg = data_rg + 0x28;
 	line_control_rg = data_rg + 0x2C;
-	*/
+	control_rg = data_rg + 0x30;
+	interrupt_mask_set_clear_rg = data_rg + 0x38;
+	raw_interrupt_status_rg = data_rg + 0x3C;
+	interrupt_clear_rg = data_rg + 0x44;
+	// Configuring UART control registers
+	uartSetControlReg(UARTEN, 0x0); // Desabling UART
+	uartSetBaudrate(baudrate); // Setting baudrate with the given value
+	uartSetControlReg(RXE, 0x1); // Enabling receive
+	uartSetControlReg(TXE, 0x1); // Enabling transmit
+	uartSetLineControlReg(WLEN, 0x3); // Set word size to 8 bits
+	uartSetInterruptClearReg(ALL, SET); // Clear all interrupts
+	uartSetInterruptMaskSetClearReg(TXIM, 0x1); // Enable interrupt mask TXIM
+	uartSetInterruptMaskSetClearReg(RXIM, 0x1); // Enable interrupt mask RXIM
+	uartSetControlReg(UARTEN, 0x1); // Enabling UART
 }
 
-// This funtion writes a char to be sent into the data register
-void uartSendChar(char c){
-	writeBits((int *) data_rg, c, 0);
+// This function transmit data via UART
+void uartTransmitData(char c){
+	writeBits(data_rg, c, 0);
 }
 
-/* 
-*	This set the line control register configuration bits into the specified field name macro. 
-*   For field names information, look at manufacturer data sheet
+// This function receive data via UART
+char uartReceiveData(){
+	while(*raw_interrupt_status_rg != 0x20); // While UART is transmiting data, wait
+	*raw_interrupt_status_rg |= 0x20;
+	return readBits(data_rg, 0x8, 0x0);
+}
+
+/*
+*	Set functions for configuration registers. It writes bits into the specified field_name macro
+*   For field_names information, look at manufacturer data sheet
 */
-void uartSetControlRg(int field_name, int bits){
-	writeBits((int *) line_control_rg, bits, field_name);
+void uartSetLineControlReg(int field_name, int bits){
+	writeBits(line_control_rg, bits, field_name);
+}
+
+void uartSetControlReg(int field_name, int bits){
+	writeBits(control_rg, bits, field_name);
+}
+
+void uartSetInterruptMaskSetClearReg(int field_name, int bits){
+	writeBits(interrupt_mask_set_clear_rg, bits, field_name);
+}
+
+void uartSetInterruptClearReg(int field_name, int bits){
+	writeBits(interrupt_mask_set_clear_rg, bits, field_name);
+}
+
+// This function sets a baudrate. It writes it into the specific registers
+void uartSetBaudrate(int baudrate){
+	float divisor = FUARTCLK / (16 * baudrate);
+	unsigned int int_part = (unsigned int) (FUARTCLK / (16 * baudrate));
+	unsigned int frac_part = (unsigned int) ((FUARTCLK % (16 * baudrate)) * 64 /(16 * baudrate));
+	writeBits(int_baudrate_rg, int_part, 0);
+	writeBits(frac_baudrate_rg, frac_part, 0);
 }
 
 // This sets to 1 a bit in a specific memory position
-void setBit(int * rgt, int n){
+void setBit(reg rgt, int n){
 	*rgt |= 1 << n;
 }
 
 // This sets to 0 a bit in a specific memory position
-void clearBit(int * rgt, int n){
+void clearBit(reg rgt, int n){
 	*rgt &= ~(1 << n);
 }
 
+// This function gets a binary value expressed by a decimal value from an specific memory position
+int readBits(reg rgt, int n_read, int position){
+	int data = *rgt, clear = ((int) pow(2,n_read)) - 1; 
+	return (data >> position) & clear;
+}
+
 // This function writes a binary value expressed by a hexadecimal value into a specific memory position
-void writeBits(int * rgt, int bits, int n){
+void writeBits(reg rgt, int bits, int n){
 	int current_bit;
 	do{
 		current_bit = bits & 0x1;
