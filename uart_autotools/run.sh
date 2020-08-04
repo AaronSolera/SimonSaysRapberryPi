e37e1a153902387d0a4c31ae92717a4cdbb0b244
olbapd <pablod.garciab@gmail.com>
2020-08-03 23:42:14 -0600

    Update run file.

--- uart_autotools/run.sh
+++ uart_autotools/run.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 . /opt/poky/2.5.1/environment-setup-cortexa7hf-neon-vfpv4-poky-linux-gnueabi
 autoreconf --install
-
-../configure --prefix="/home/<Su usuario de linux>/<Ruta de la carpeta usr>" --host=arm-poky-linux-gnueabi
 cd build
+../configure --prefix="/home/olbapd/Documents/Embebidos/Proyecto3/SimonSaysRapberryPi/uart_autotools/build/usr" --host=arm-poky-linux-gnueabi
 make 
 make install
 cd usr && ls
--- uart_autotools/src/main.c
+++ uart_autotools/src/main.c
@@ -1,17 +1,17 @@
-//#include <uartdriver.h>
+#include <uartdriver.h>
 #include <string.h>
 #include <stdio.h>
 int main(int argc, char const *argv[]){
 	printf("Uart Driver recieved instruction");
- //  if (strcmp(argv[1], "uartInit") == 0){
-	// 	uartInit(9600);
- //    printf("Uart Driver initiated");
-	// } 
- //  else if (strcmp(argv[1], "uartReceiveData") == 0){
-	// 	printf("%c", uartReceiveData());
-	// } 
- //  else if (strcmp(argv[1], "uartTransmitData") == 0){
-	// 	uartTransmitData((char)*argv[2]);
-	// }
+  if (strcmp(argv[1], "uartInit") == 0){
+		uartInit(9600);
+    printf("Uart Driver initiated");
+	} 
+  else if (strcmp(argv[1], "uartReceiveData") == 0){
+		printf("%c", uartReceiveData());
+	} 
+  else if (strcmp(argv[1], "uartTransmitData") == 0){
+   	uartTransmitData((char)*argv[2]);
+	}
 	return 0;
 }
